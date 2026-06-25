a4a8f88a92d49789df3ef5b33a1953b09e261bf6
sawyanpaing88 <54151791+sawyanpaing88@users.noreply.github.com>
2026-06-25 17:47:29 +0630

    Update app.py

--- app.py
+++ app.py
@@ -891,7 +891,7 @@ elif page_selection == "➕ Build New Quotation Module":
             <style>
                 @page {{
                     size: A4;
-                    margin: 15mm 15mm 0mm 15mm;
+                    margin: 15mm 15mm 20mm 0mm;
                     @bottom-right {{
                         content: "Page " counter(page) " of " counter(pages);
                         font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
